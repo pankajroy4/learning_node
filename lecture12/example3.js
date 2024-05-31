8ec6463a55816113e10a2867126d9b16833f34f8
pankajroy4 <pankajroy842002@gmail.com>
2024-05-31 16:59:50 +0530

    lecture 12 Done

--- lecture12/example3.js
+++ lecture12/example3.js
@@ -21,10 +21,42 @@ For example:
 Why we need to learn about events?
 --------------------------------------
 In node , we have a class called "http" that we will use to build our own web server.
-So we listen on a given port, and every time we receive a request on that port, that http class raises an event.
+So we listen on a given port, and every time we receive a request from client on that port, that http class raises an event.
 
 We capture that event and generate the response.
 
+Browser/client will send a request, which will be accepted by http and it raise an event then we capture this event and generate the response and return to browser/client.
 
-Time: 40 minutes.
+when we will use 'http' later, then event will be fired by http and we have to capture it and generate the response. So in this case 'http' fire event for us.
+But as of now we will use core module of node.js called 'events' module.
+============================================================
+
+The events Module
+=========================================================
+The 'events' module is core module of node.js. The events module allows us to easily create i.e raise and handle custom events in Node.js.
+
+This module includes the 'EventEmitter' class, which is used to raise and handle the events.
+The event module default exports the class 'EventEmitter' as module.exports = EventEmitter. So when we require this module then we will get EventEmitter class as:
+  const EventEmitter = require('events');
+
+As of now we will use events module so we ouselves have to fire event, capture it and generate the response.
+
+In node , event raising is called : event emit, and code for  handling event is cllled : event listener.
+
+Almost the whole of the Node.js core API is built around the events module, which emits named events that cause function(also called as listeners) to be called.
+
+In events module , the most important class in EventEmitter class.
+This class have many methods. The six mainly used methods are:
+
+  1. addListener(event, listener) : Adds a listener to the listeners array for the specified event.
+
+  2. on(event. listener) : It can also be called as an alias of addListener()
+
+  3. once(event, listener) : Adds a one-time listener for the event. This listener is invoked only the next time the event is fired, after which it is removed.
+
+  4. emit(event, [arg1], [arg2], [...]) : Raises the specified events with the supplied arguments.
+
+  5. removeListener(event, litener) : Removes a listener from the listener array for the specified event.
+
+  6. removeAllListeners([event]) : Removes all listeners, of the specified event.
 */
